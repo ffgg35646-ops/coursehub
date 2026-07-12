d6d1b5d7dc1feeb3d606423c606513a9e722ef85
mohamed ali <ffgg35646@gmail.com>
2026-07-11 18:22:04 -0700

    categories.ts

---
+++ src/data/categories.ts
@@ -0,0 +1,48 @@
+import type { Category } from "@/types";
+
+export const categories: Category[] = [
+  {
+    id: "1",
+    name: "Web Development",
+    slug: "web-development",
+    image:
+      "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+  },
+  {
+    id: "2",
+    name: "Backend Development",
+    slug: "backend-development",
+    image:
+      "https://images.unsplash.com/photo-1558494949-ef010cbdcc31",
+  },
+  {
+    id: "3",
+    name: "Mobile Development",
+    slug: "mobile-development",
+    image:
+      "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c",
+  },
+  {
+    id: "4",
+    name: "UI/UX Design",
+    slug: "ui-ux-design",
+    image:
+      "https://images.unsplash.com/photo-1558655146-d09347e92766",
+  },
+  {
+    id: "5",
+    name: "Database",
+    slug: "database",
+    image:
+      "https://images.unsplash.com/photo-1544383835-bda2bc66a55d",
+  },
+  {
+    id: "6",
+    name: "DevOps",
+    slug: "devops",
+    image:
+      "https://images.unsplash.com/photo-1667372393119-3d4c48d07fc9",
+  },
+];
+
+export default categories;
